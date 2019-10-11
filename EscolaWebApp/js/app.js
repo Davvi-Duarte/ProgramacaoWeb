b2b32a8c3a575414d9ef04bf91b1d6a1507c10d2
IFPB <davvi.rodrigues100@gmail.com>
2019-10-11 09:03:56 -0300

    atualização perca do proj

--- EscolaWebApp/js/app.js
+++ EscolaWebApp/js/app.js
@@ -1,28 +1,25 @@
 // Inicializar o módulo.
 let nomeApp = 'EscolaWebApp';
 let modulos = [];
+
 var app = angular.module(nomeApp, modulos);
 
-// Estrutura básica para uma função no controlador.
-var meuPrimeiroController = function($scope){
-  $scope.logradouro = "Outro valor";
+//Estrutura básica para uma função no controlador.
+//toda variável associada ao $scope: se eu modificar no JS, modifico na tela.
+var MeuPrimeiroController = function($scope){
+  $scope.logradouro = 'Outro valor';
   $scope.numero1 = 0;
   $scope.numero2 = 0;
-  let nome = "João";
-  $scope.nome = nome;
 
-  $scope.somar = function(numero1, numero2) {
-    $scope.resultado = somar(numero1, numero2);
-  }
-
-  function somar(numero1, numero2) { 
-    return numero1 + numero2;
+  $scope.somar = function(numero1, numero2){
+    $scope.resultado = numero1 + numero2;
   }
 }
 
-app.controller('MeuPrimeiroController', meuPrimeiroController);
+app.controller('MeuPrimeiroController', MeuPrimeiroController);
 
-var meuSegundoController = function($scope){
+var MeuSegundoController = function($scope){
+  $scope.logradouro = 'Outro valor';
 }
 
-app.controller('MeuSegundoController', meuSegundoController);
+app.controller('MeuSegundoController', MeuSegundoController);
